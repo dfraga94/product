86161c18ddf1b73eb577ece3efb1398749797029
Darwin Fraga <darwinsvladi@hotmail.com>
2022-12-10 08:26:34 -0500

    creando capa repository

---
+++ src/main/java/ec/edu/itsqmet/product/dto/ProductDTO.java
@@ -0,0 +1,16 @@
+package ec.edu.itsqmet.product.dto;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "product")
+public class ProductDTO {
+	@Id
+	private int id;
+	private String name;
+	private BigDecimal price;
+	private int stock;
+	private Boolean active;
+}
